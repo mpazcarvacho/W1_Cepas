0ec043337686812b541393a12c2b934957919ffa
mariapaz <mpazcarvacho@gmail.com>
2020-11-10 13:30:59 -0300

    bootstrap added to new score form

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   resources :assemblies
   resources :strains
   resources :wines
+
+  root "wines#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
